e424e4620d60a23d4ba4aeb5a050fd74bf10d054
Jake Soenneker <jake@soeneker.com>
2024-12-20 17:34:21 +0000

    Tests

---
+++ test/Soenneker.Extensions.LoggerConfiguration.Hangfire.Tests/HangfireLoggerConfigExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.LoggerConfiguration.Hangfire.Tests;
+
+public class HangfireLoggerConfigExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
